b75661f565588dfee948c68baef72285ac53563d
Gurjot <gurjotsingh1995@yahoo.com>
2018-08-03 00:12:19 +0530

    Updated methods, now works for all LED's

--- KeyboardVisualizerCommon/AlienFXSDK.cpp
+++ KeyboardVisualizerCommon/AlienFXSDK.cpp
@@ -26,8 +26,15 @@ LFX2RESET resetFunction;
 LFX2UPDATE updateFunction;
 LFX2LIGHT lightFunction;
 LFX2ACTIONCOLOR lightActionColorFunction;
+LFX2SETLIGHTACTIONCOLOR setlightActionColor;
 LFX2ACTIONCOLOREX lightActionColorExFunction;
 LFX2SETTIMING setTiming;
+LFX2GETNUMDEVICES getNumDevicesFunction;
+LFX2GETDEVDESC getDeviceDescriptionFunction;
+LFX2GETNUMLIGHTS getNumLightsFunction;
+LFX2SETLIGHTCOL setLightColorFunction;
+LFX2GETLIGHTCOL getLightColorFunction;
+LFX2GETLIGHTDESC getLightDescriptionFunction;
 //LFX_RESULT LFX_GetTiming(const int x);
 LFX_RESULT result;
 
@@ -43,6 +50,12 @@ void AlienFXSDK::Initialize() {
 		lightActionColorFunction = (LFX2ACTIONCOLOR) GetProcAddress(hLibrary, LFX_DLL_ACTIONCOLOR);
 		lightActionColorFunction = (LFX2ACTIONCOLOR) GetProcAddress(hLibrary, LFX_DLL_ACTIONCOLOR);
 		lightActionColorExFunction = (LFX2ACTIONCOLOREX) GetProcAddress(hLibrary, LFX_DLL_ACTIONCOLOREX);
+		setlightActionColor = (LFX2SETLIGHTACTIONCOLOR) GetProcAddress(hLibrary, LFX_DLL_SETLIGHTACTIONCOLOR);
+		getNumDevicesFunction = (LFX2GETNUMDEVICES) GetProcAddress(hLibrary, LFX_DLL_GETNUMDEVICES);
+		getDeviceDescriptionFunction = (LFX2GETDEVDESC) GetProcAddress(hLibrary, LFX_DLL_GETDEVDESC);
+		getNumLightsFunction = (LFX2GETNUMLIGHTS) GetProcAddress(hLibrary, LFX_DLL_GETNUMLIGHTS);
+		setLightColorFunction = (LFX2SETLIGHTCOL) GetProcAddress(hLibrary, LFX_DLL_SETLIGHTCOL);
+		getLightColorFunction = (LFX2GETLIGHTCOL) GetProcAddress(hLibrary, LFX_DLL_GETLIGHTCOL);
 		setTiming = (LFX2SETTIMING) GetProcAddress(hLibrary, LFX_DLL_SETTIMING);
 		result = initFunction();
 	}
@@ -51,20 +64,52 @@ void AlienFXSDK::Initialize() {
 
 bool AlienFXSDK::SetLEDs(COLORREF pixels[64][256]) { 
 	if (result == LFX_SUCCESS) {
-		result = setTiming(100);
+		//result = setTiming(100);
 		result = resetFunction();
+
+		unsigned int numDevs = 0;
+		result = getNumDevicesFunction(&numDevs);
+
+		for (unsigned int devIndex = 0; devIndex < numDevs; devIndex++) {
+			unsigned int numLights = 0;
+			result = getNumLightsFunction(devIndex, &numLights);
+
+			//Add the number of colors you want for each LED, I am taking 2 for example
+			LFX_COLOR color1, color2;
+
+			color1.red = 255;
+			color1.green = 0;
+			color1.blue = 0;
+			color1.brightness = 255;
+
+			color2.red = 155;
+			color2.green = 120;
+			color2.blue = 255;
+			color2.brightness = 255;
+
+			//Example which sets 2 colors across all LED's for each odd and even index
+			for (unsigned int lightIndex = 0; lightIndex < numLights; lightIndex++)
+				setLightColorFunction(devIndex, lightIndex, (lightIndex % 2 == 0) ? &color1 : &color2);
+			
+		}
+		result = updateFunction();
+
+		//Use this code to release resources such as when program exits and set AlienFX to default state
+		//result = releaseFunction();
+
+		//-------------------Old code------------------
+
 		//COLORREF single_color = pixels[ROW_IDX_SINGLE_COLOR][0];
 
 		//Solid Colors Example on Left Keyboard Zone
-		result = lightFunction(LFX_FRONT_LOWER_LEFT, LFX_GREEN | LFX_FULL_BRIGHTNESS);
-	
+		//result = lightFunction(LFX_FRONT_LOWER_LEFT, LFX_GREEN | LFX_FULL_BRIGHTNESS);
 
 		//This is Pulse Effect example on Center Zone
-		result = lightActionColorFunction(LFX_FRONT_LOWER_CENTER, LFX_ACTION_PULSE, LFX_CYAN | LFX_FULL_BRIGHTNESS);
-		Sleep(5000);
+		//result = lightActionColorFunction(LFX_FRONT_LOWER_CENTER, LFX_ACTION_PULSE, LFX_CYAN | LFX_FULL_BRIGHTNESS);
+		//Sleep(5000);
 		//This is Morph Effect (transisiton of one color to another) Example on Right Zone
-		result = lightActionColorExFunction(LFX_FRONT_LOWER_RIGHT, LFX_ACTION_MORPH, LFX_RED | LFX_FULL_BRIGHTNESS, LFX_BLUE | LFX_FULL_BRIGHTNESS);
-		result = updateFunction();
+		//result = lightActionColorExFunction(LFX_FRONT_LOWER_RIGHT, LFX_ACTION_MORPH, LFX_RED | LFX_FULL_BRIGHTNESS, LFX_BLUE | LFX_FULL_BRIGHTNESS);
+		
 		return true;
 	} else {
 		return false;
